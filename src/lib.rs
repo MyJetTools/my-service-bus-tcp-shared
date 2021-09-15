933b92b336e3294424207c3582ef01d97f47807e
Andrey <amigin@gmail.com>
2021-09-15 20:26:06 +0300

    Exposed Connection Attributes

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,13 @@ pub mod legacy;
 pub mod tcp_contract_to_string;
 pub mod tcp_message_id;
 
+mod connection_attrs;
 mod my_sb_socket_error;
 mod packet_versions;
 mod socket_reader;
 mod tcp_contracts;
 
+pub use connection_attrs::ConnectionAttributes;
 pub use my_sb_socket_error::MySbSocketError;
 pub use packet_versions::PacketVersions;
 pub use socket_reader::TSocketReader;
